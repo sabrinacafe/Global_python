d86d86759eff747f62817e59eee51cda66c7244d
Sabrina da Motta Café <sabis476@gmail.com>
2023-11-30 03:05:34 -0300

    Correções: posição listas cadast e opção Categoria

--- menu_opcoes.py
+++ menu_opcoes.py
@@ -1,7 +1,8 @@
-def opcao_user():
-    acao = True
+pacientes = []
+fisioterapeutas = []
 
-    while acao:
+def opcao_user():
+    while True:
         print('Bem vindo ao Menu Hapvida')
         print('1. Categorias')
         print('2. Agenda')
@@ -46,66 +47,27 @@ def opcao_categorias():
 
         select_categoria = input('Selecione uma opção: ')
 
-        if select_categoria == '1':
-            if select_categoria == '1':
-                print('''
-                   Quiropraxia é uma abordagem de saúde que trata problemas musculoesqueléticos,
-                   especialmente na coluna vertebral, usando ajustes manuais. Busca aliviar dores
-                   e melhorar a função do sistema nervoso.
-                   ''')
-            elif select_categoria == '2':
-                print('''
-                   Massoterapia é uma prática terapêutica que envolve a aplicação de técnicas
-                   de massagem para promover relaxamento, alívio de tensões musculares e melhorar
-                   o bem-estar físico e emocional. Ela é utilizada para reduzir o estresse, aliviar
-                   dores musculares, melhorar a circulação sanguínea e promover a saúde geral.
-                   ''')
-            elif select_categoria == '3':
-                print('''
-                    Fisioterapia respiratória é uma especialidade da fisioterapia que visa tratar
-                    e prevenir distúrbios respiratórios. Utiliza técnicas e exercícios para melhorar
-                    a função pulmonar, facilitar a respiração e ajudar pacientes com condições como
-                    asma, doença pulmonar obstrutiva crônica (DPOC) e outras doenças respiratórias.
-                    ''')
-            elif select_categoria == '4':
-                print('''
-                    Dores nas costas podem ser causadas por diversos fatores. Agende com um de nossos especialistas para
-                    que ele possa indicar a melhor forma de tratamento.
-                    ''')
-            elif select_categoria == '5':
-                print('''
-                    A fisioterapia pediátrica é uma especialidade que trata crianças, utilizando
-                    técnicas e exercícios específicos para promover o desenvolvimento motor, melhorar
-                    a função física e tratar condições pediátricas como atrasos no desenvolvimento,
-                    lesões neuromusculares e problemas ortopédicos. Ela visa melhorar a qualidade de vida
-                    e a independência funcional das crianças.
-                    ''')
-            elif select_categoria == '6':
-                print('''
-                    O tratamento com ventosas é uma técnica terapêutica que envolve a aplicação de
-                    copos de sucção na pele para criar pressão negativa. Isso promove o aumento do
-                    fluxo sanguíneo local, alivia a tensão muscular e pode ser usado para tratar dores
-                    musculares, melhorar a circulação e promover a recuperação de lesões.
-                    ''')
-            elif select_categoria == '7':
-                print('''
-                    A fisioterapia geriátrica é uma especialidade que se concentra no tratamento de
-                    idosos, visando melhorar a mobilidade, equilíbrio, força e independência funcional.
-                    Ela ajuda a prevenir e tratar condições relacionadas à idade, promovendo a qualidade
-                    de vida e a autonomia dos idosos.
-                    ''')
-            elif select_categoria == '8':
-                print('''
-                    Reabilitação vestibular é um conjunto de exercícios e técnicas utilizados para tratar
-                    distúrbios do sistema vestibular, que controla o equilíbrio e a orientação espacial. Essa
-                    terapia é empregada para aliviar tonturas, vertigens e problemas de equilíbrio, visando
-                    melhorar a função vestibular e a qualidade de vida do paciente.
-                    ''')
-            elif select_categoria == '9':
-                print('Voltando ao Menu Inicial')
-                break
-            else:
-                print('Opção inválida')
+        if select_categoria == '9':
+            print('Voltando ao Menu Inicial')
+            break
+        elif select_categoria.isdigit() and 1 <= int(select_categoria) <= 8:
+            exibir_info_categoria(select_categoria)
+        else:
+            print('Opção inválida')
+
+def exibir_info_categoria(categoria):
+    categorias_info = {
+        '1': 'Quiropraxia é uma abordagem de saúde que trata problemas musculoesqueléticos, especialmente na coluna vertebral, usando ajustes manuais. Busca aliviar dores e melhorar a função do sistema nervoso.',
+        '2': 'Massoterapia é uma prática terapêutica que envolve a aplicação de técnicas de massagem para promover relaxamento, alívio de tensões musculares e melhorar o bem-estar físico e emocional. Ela é utilizada para reduzir o estresse, aliviar dores musculares, melhorar a circulação sanguínea e promover a saúde geral.',
+        '3': 'Fisioterapia respiratória é uma especialidade da fisioterapia que visa tratar e prevenir distúrbios respiratórios. Utiliza técnicas e exercícios para melhorar a função pulmonar, facilitar a respiração e ajudar pacientes com condições como asma, doença pulmonar obstrutiva crônica (DPOC) e outras doenças respiratórias.',
+        '4': 'Dores nas costas podem ser causadas por diversos fatores. Agende com um de nossos especialistas para que ele possa indicar a melhor forma de tratamento.',
+        '5': 'A fisioterapia pediátrica é uma especialidade que trata crianças, utilizando técnicas e exercícios específicos para promover o desenvolvimento motor, melhorar a função física e tratar condições pediátricas como atrasos no desenvolvimento, lesões neuromusculares e problemas ortopédicos. Ela visa melhorar a qualidade de vida e a independência funcional das crianças.',
+        '6': 'O tratamento com ventosas é uma técnica terapêutica que envolve a aplicação de copos de sucção na pele para criar pressão negativa. Isso promove o aumento do fluxo sanguíneo local, alivia a tensão muscular e pode ser usado para tratar dores musculares, melhorar a circulação e promover a recuperação de lesões.',
+        '7': 'A fisioterapia geriátrica é uma especialidade que se concentra no tratamento de idosos, visando melhorar a mobilidade, equilíbrio, força e independência funcional. Ela ajuda a prevenir e tratar condições relacionadas à idade, promovendo a qualidade de vida e a autonomia dos idosos.',
+        '8': 'Reabilitação vestibular é um conjunto de exercícios e técnicas utilizados para tratar distúrbios do sistema vestibular, que controla o equilíbrio e a orientação espacial. Essa terapia é empregada para aliviar tonturas, vertigens e problemas de equilíbrio, visando melhorar a função vestibular e a qualidade de vida do paciente.'
+    }
+
+    print(categorias_info[categoria])
 
 def opcao_agenda():
     while True:
@@ -147,9 +109,6 @@ def opcao_empresa():
             break
         else:
             print('Opção inválida')
-
-pacientes = []
-fisioterapeutas = []
 
 def cadastrar_paciente():
     print('Cadastro de Paciente')
